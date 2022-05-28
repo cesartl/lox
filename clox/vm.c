2bc448810f69c4b0c88a136c9a79bd50860466f4
Cesar TL <cesar.tronlozai@gmai.com>
2022-05-28 12:11:52 +0100

    clox21

--- clox/vm.c
+++ clox/vm.c
@@ -33,11 +33,13 @@ void initVM()
 {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.globals);
     initTable(&vm.strings);
 }
 
 void freeVM()
 {
+    freeTable(&vm.globals);
     freeTable(&vm.strings);
     freeObjects();
 }
@@ -62,6 +64,22 @@ static bool isFalsey(Value value) {
   return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
 }
 
+static void printStrings(){
+    Table* table = &vm.strings;
+    if(table->count == 0) return;
+    printf("          ");
+    printf("Interned strings ");
+    for (int i = 0; i < table->capacity; i++) {
+        Entry* entry = &table->entries[i];
+        if(table->count == 0) return;
+        if (entry->key != NULL) {
+            printf("\"%s\"", entry->key->chars);
+            printf(" ");
+        }
+    }
+  printf("\n");
+}
+
 static void concatenate() {
   ObjString* b = AS_STRING(pop());
   ObjString* a = AS_STRING(pop());
@@ -80,6 +98,7 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op) \
     do { \
       if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
@@ -102,67 +121,97 @@ static InterpretResult run()
             printf(" ]");
         }
         printf("\n");
+        // printStrings();
         disassembleInstruction(vm.chunk,
                                (int)(vm.ip - vm.chunk->code));
 #endif
-        uint8_t instruction;
-        switch (instruction = READ_BYTE())
-        {
+    uint8_t instruction;
+    switch (instruction = READ_BYTE())
+    {
         case OP_CONSTANT:
         {
             Value constant = READ_CONSTANT();
             push(constant);
             break;
         }
-      case OP_NIL: push(NIL_VAL); break;
-      case OP_TRUE: push(BOOL_VAL(true)); break;
-      case OP_FALSE: push(BOOL_VAL(false)); break;
-      case OP_EQUAL: {
-        Value b = pop();
-        Value a = pop();
-        push(BOOL_VAL(valuesEqual(a, b)));
-        break;
-      }
-      case OP_GREATER:  BINARY_OP(BOOL_VAL, >); break;
-      case OP_LESS:     BINARY_OP(BOOL_VAL, <); break;
-      case OP_ADD: {
-        if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
-          concatenate();
-        } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
-          double b = AS_NUMBER(pop());
-          double a = AS_NUMBER(pop());
-          push(NUMBER_VAL(a + b));
-        } else {
-          runtimeError(
-              "Operands must be two numbers or two strings.");
-          return INTERPRET_RUNTIME_ERROR;
-        }
-        break;
-      }
-      case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
-      case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
-      case OP_DIVIDE:   BINARY_OP(NUMBER_VAL, /); break;
-      case OP_NOT:
-        push(BOOL_VAL(isFalsey(pop())));
-        break;      
-      case OP_NEGATE:
-            if(!IS_NUMBER(peek(0))){
-                runtimeError("Operand must be a number");
+        case OP_NIL: push(NIL_VAL); break;
+        case OP_TRUE: push(BOOL_VAL(true)); break;
+        case OP_FALSE: push(BOOL_VAL(false)); break;
+        case OP_POP: pop(); break;
+        case OP_GET_GLOBAL: {
+            ObjString* name = READ_STRING();
+            Value value;
+            if (!tableGet(&vm.globals, name, &value)) {
+                runtimeError("Undefined variable '%s'.", name->chars);
                 return INTERPRET_RUNTIME_ERROR;
             }
-            push(NUMBER_VAL(-AS_NUMBER(pop())));
+            push(value);
             break;
-        case OP_RETURN:
-        {
+        }        
+        case OP_DEFINE_GLOBAL: {
+            ObjString* name = READ_STRING();
+            tableSet(&vm.globals, name, peek(0));
+            pop();
+            break;
+        }
+        case OP_SET_GLOBAL: {
+            ObjString* name = READ_STRING();
+            if (tableSet(&vm.globals, name, peek(0))) {
+                tableDelete(&vm.globals, name); 
+                runtimeError("Undefined variable '%s'.", name->chars);
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            break;
+        }        
+        case OP_EQUAL: {
+            Value b = pop();
+            Value a = pop();
+            push(BOOL_VAL(valuesEqual(a, b)));
+            break;
+        }
+        case OP_GREATER:  BINARY_OP(BOOL_VAL, >); break;
+        case OP_LESS:     BINARY_OP(BOOL_VAL, <); break;
+        case OP_ADD: {
+            if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+            concatenate();
+            } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+            double b = AS_NUMBER(pop());
+            double a = AS_NUMBER(pop());
+            push(NUMBER_VAL(a + b));
+            } else {
+            runtimeError(
+                "Operands must be two numbers or two strings.");
+            return INTERPRET_RUNTIME_ERROR;
+            }
+            break;
+        }
+        case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+        case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
+        case OP_DIVIDE:   BINARY_OP(NUMBER_VAL, /); break;
+        case OP_NOT:
+            push(BOOL_VAL(isFalsey(pop())));
+            break;      
+        case OP_NEGATE:
+                if(!IS_NUMBER(peek(0))){
+                    runtimeError("Operand must be a number");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                push(NUMBER_VAL(-AS_NUMBER(pop())));
+                break;
+        case OP_PRINT: {
             printValue(pop());
             printf("\n");
-            return INTERPRET_OK;
+            break;
         }
+        case OP_RETURN: {
+            // Exit interpreter.
+            return INTERPRET_OK;        
         }
     }
-
+}
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef READ_STRING
 #undef BINARY_OP
 }
 
